01729e1edd0daf84d79edb063ddb9bf64a22e26a
LilyGO <31160602+LilyGO@users.noreply.github.com>
2018-10-09 09:36:17 +0800

    Update SSD1306SimpleDemo.ino

--- examples/SSD1306SimpleDemo/SSD1306SimpleDemo.ino
+++ examples/SSD1306SimpleDemo/SSD1306SimpleDemo.ino
@@ -58,7 +58,7 @@
 // SH1106Brzo  display(0x3c, D3, D5);
 
 // Initialize the OLED display using Wire library
-SSD1306  display(0x3c, 5, 4); //5,4  21,22
+SSD1306  display(0x3c, 5, 4); //5,4  21,22 4，15
 // SH1106 display(0x3c, D3, D5);
 
 
